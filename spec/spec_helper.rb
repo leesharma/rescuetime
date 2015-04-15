331bc5d9af8c668ad0f085da86e1c20a5d2ddcf8
Lee <lee@leesharma.com>
2015-04-15 15:44:27 -0400

    vcr ignore code climate - fix: VCR::Errors::UnhandledHTTPRequestError raised by Code Climate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ require 'csv'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/cassettes'
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 
   # Put '<RESCUETIME_API_KEY>' so API key is not committed to source control
   config.filter_sensitive_data('<RESCUETIME_API_KEY>') { 'AK' }
