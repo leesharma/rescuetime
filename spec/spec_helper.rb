7b9bf84ebc304c5490bf3b4400a9db8211dd85a3
Lee <lee@leesharma.com>
2015-04-14 22:44:29 -0400

    initialize codeclimate test reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rescuetime'
 
@@ -13,5 +16,6 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/cassettes'
   config.hook_into :webmock
 
+  # Put '<RESCUETIME_API_KEY>' so API key is not committed to source control
   config.filter_sensitive_data('<RESCUETIME_API_KEY>') { Secret::API_KEY }
 end
