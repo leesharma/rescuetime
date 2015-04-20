ada9a5d595415d8e6c61b338faf2edd4a486a632
Lee <lee@leesharma.com>
2015-04-20 13:26:41 -0400

    add restrict_kind with all options - add: cassettes - add: specs for each possible value - add: yard comments

--- lib/rescuetime/activities.rb
+++ lib/rescuetime/activities.rb
@@ -7,7 +7,7 @@ module Rescuetime
     # Base URL for RescueTime Data Analytics API endpoint
     BASE_URL = 'https://www.rescuetime.com/anapi/data'
 
-    # Returns array of all activities.
+    # Returns array of all activities. The default request is equivalent to @client.activities restrict_kind: 'activity'
     #
     # @example
     #   @client.activities.class     # => Array
@@ -22,15 +22,33 @@ module Rescuetime
     #   #       :category=>"General Software Development",
     #   #       :productivity=>2
     #   #     }
+    #
+    #   @client.activities({ restrict_kind: 'productivity' })
+    #   # =>  [
+    #   #       { :rank=>1, :time_spent_seconds=>6956, :number_of_people=>1, :productivity=>2 },
+    #   #       { :rank=>2, :time_spent_seconds=>2635, :number_of_people=>1, :productivity=>-2 },
+    #   #       { :rank=>3, :time_spent_seconds=>2415, :number_of_people=>1, :productivity=>1 },
+    #   #       { :rank=>4, :time_spent_seconds=>1210, :number_of_people=>1, :productivity=>0 },
+    #   #       { :rank=>5, :time_spent_seconds=>93, :number_of_people=>1, :productivity=>-1 }
+    #   #     ]
+    #
+    # @param [Hash] options Query parameters to be passed to RescueTime
+    # @option options [String] :restrict_kind
+    #   Restricts the level of detail and/or kind of report returned
+    #   1. 'overview': sums statistics for all activities into their top level category
+    #   2. 'category': sums statistics for all activities into their sub category
+    #   3. 'activity': sums statistics for individual applications / web sites / activities
+    #   4. 'productivity': productivity calculation
+    #   5. 'efficiency': efficiency calculation (not applicable in "rank" perspective)
     # @return [Array<Hash>]
     # @since v0.1.0
-    def activities
+    def activities(options={})
       connection = Faraday.new(Rescuetime::Activities::BASE_URL)
-      options = { key: @api_key,
-                  format: 'csv',
-                  version: 0,
-                  operation: 'select' }
-      response = connection.get '', options
+      params = { key: @api_key,
+                 format: 'csv',
+                 version: 0,
+                 operation: 'select' }
+      response = connection.get '', params.merge(options)
 
       activities_from_csv response.body
     end
--- spec/unit/activities_spec.rb
+++ spec/unit/activities_spec.rb
@@ -16,5 +16,62 @@ describe Rescuetime::Activities do
         expect(@client.activities[0]).to be_instance_of(Hash)
       end
     end
+    describe 'restrict_kind:' do
+      describe "'overview'" do
+        it 'restricts activity detail to overview' do
+          VCR.use_cassette('api_client.activities restrict_kind: overview',
+                           match_requests_on: [:host, :path], record: :none) do
+            activities = @client.activities restrict_kind: 'overview'
+            activity_keys = activities[0].keys
+            expect(activity_keys).to include(:category)
+            expect(activity_keys).not_to include(:activity)
+            expect(activity_keys).not_to include(:productivity)
+          end
+        end
+      end
+
+      describe "'category'" do
+        it 'restricts activity detail to category' do
+          VCR.use_cassette('api_client.activities restrict_kind: category',
+                           match_requests_on: [:host, :path], record: :none) do
+            activities = @client.activities restrict_kind: 'category'
+            activity_keys = activities[0].keys
+            expect(activity_keys).to include(:category)
+            expect(activity_keys).not_to include(:activity)
+            expect(activity_keys).not_to include(:productivity)
+          end
+        end
+      end
+
+      describe "'activity'" do
+        it 'restricts activity detail to activity' do
+          VCR.use_cassette('api_client.activities restrict_kind: activity',
+                           match_requests_on: [:host, :path], record: :none) do
+            activities = @client.activities restrict_kind: 'activity'
+            activity_keys = activities[0].keys
+            expect(activity_keys).to include(:activity)
+          end
+        end
+      end
+
+      describe "'productivity'" do
+        it 'returns productivity report' do
+          VCR.use_cassette('api_client.activities restrict_kind: productivity',
+                           match_requests_on: [:host, :path], record: :none) do
+            activities = @client.activities restrict_kind: 'productivity'
+            activity_keys = activities[0].keys
+            expect(activity_keys).to include(:productivity)
+            expect(activity_keys).not_to include(:activity)
+            expect(activity_keys).not_to include(:category)
+          end
+        end
+      end
+
+      describe "'efficiency'" do
+        it 'returns efficiency report (only valid with by: \'time\')' do
+          # TODO: Need to implement :by before this will work
+        end
+      end
+    end
   end
 end
